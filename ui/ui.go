773df6caf894249c1d55938a3e8ea8b29842bb84
liudawei <ldw2looper@dingtalk.com>
2023-08-09 11:23:36 +0800

    add rand string

--- ui/ui.go
+++ ui/ui.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/getlantern/systray"
 	"github.com/ncruces/zenity"
+	"math/rand"
 	"util-ui/domian"
 )
 
@@ -14,11 +15,14 @@ func InitUi() {
 func onReady() {
 	systray.SetIcon(domian.MainLogo)
 	strLengthItem := systray.AddMenuItem("字符长度计算", "")
-
+	randStrItem := systray.AddMenuItem("随机字符串生成", "")
 	for {
 		select {
 		case <-strLengthItem.ClickedCh:
 			strLength()
+
+		case <-randStrItem.ClickedCh:
+			randStr()
 		}
 	}
 }
@@ -39,4 +43,39 @@ func strLength() {
 
 	zenity.Info(fmt.Sprintf("长度为：%d", length), zenity.Title("字符长度统计"),
 		zenity.Width(200), zenity.Height(100), zenity.NoIcon)
+}
+
+// 生成随机字符串
+func randStr() {
+	inputStr, _ := zenity.Entry("需求的字符长度:",
+		zenity.Title("随机字符串生成"))
+
+	l := stringToInt(inputStr)
+	if l <= 0 {
+		return
+	}
+
+	randStrData := randomString(l)
+
+	zenity.Info(randStrData, zenity.Title("随机字符串生成"),
+		zenity.Width(200), zenity.Height(100), zenity.NoIcon)
+}
+
+// 随机生成字符串
+func randomString(n int) string {
+	var letters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+	result := make([]byte, n)
+	for i := range result {
+		result[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(result)
+}
+
+// string 转 int
+func stringToInt(str string) int {
+	var result int
+	for i := 0; i < len(str); i++ {
+		result = result*10 + int(str[i]-'0')
+	}
+	return result
 }
